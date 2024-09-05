72e4f10fde0e0774332a5cd044cd1bee939d7244
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-05 09:57:55 +0200

    commit

---
+++ exercices/exercice02/exercice02.java
@@ -0,0 +1,7 @@
+package exercices.exercice02;
+
+public class exercice02 {
+public static void main(String[] args){
+ System.out.println("bonjour tout le monde");
+}
+}
---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,8 @@
+public class TypesDeBase{
+    public static void main(String[] args) {
+        int age = 5;
+        String nom = "alexandre";
+        System.out.println("je m'appelle " + nom + " et j'ai " + age + " ans");
+        System.out.println("dans 10 ans j'aurais " + (age + 10) );
+    }
+}
