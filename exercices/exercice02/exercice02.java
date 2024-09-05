22ff4d40a5365da75beeb4e45d86fcfc9c689ff0
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-05 10:04:08 +0200

    exercice 2 fini

--- exercices/exercice02/exercice02.java
+++ exercices/exercice02/exercice02.java
@@ -1,7 +1,7 @@
-package exercices.exercice02;
+package exercice02;
 
 public class exercice02 {
-public static void main(String[] args){
- System.out.println("bonjour tout le monde");
-}
+    public static void main(String[] args) {
+        System.out.println("bonjour tout le monde");
+    }
 }
