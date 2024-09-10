e4283db8d74a767ad33a24fcc6b28d9798c6b83f
gaspar-alexandre-emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-10 19:20:06 +0200

    devoir01 terminé

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,20 @@
+public class devoir01 {
+    public static void main(String[] args) {
+        
+        int monAge = 20;
+
+        int estMajeur = 18;
+
+        System.out.println("je m'apelle alexandre");
+
+        System.out.println("mon age est de " + monAge);
+
+        if (monAge <= estMajeur) {
+            System.out.println("je suis mineur");}
+        if (monAge >= estMajeur) {
+            System.out.println("je suis majeur");
+        }
+                
+            }
+            
+    }
