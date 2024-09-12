d8e2350019cf3ee4f8f6a92c6209245ad71899d7
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-12 09:45:11 +0200

    exercice04 terminé

---
+++ exercices/exercice04/EchangeValeurs.java
@@ -0,0 +1,19 @@
+package exercice04;
+
+public class EchangeValeurs {
+    public static void main(String[] args) {
+        
+        int variable1 = 2;
+
+        int variable2 = 1;
+
+        System.out.println("la valeur de ma variable1 est: " +variable1+ " la valeur de la variable2 est: " +variable2);
+
+        variable1 = 1;
+
+        variable2 = 2;
+
+        System.out.println("la valeur de ma variable1 est: " +variable1+ " la valeur de ma variable2 est: " +variable2);
+    }
+    }
+
