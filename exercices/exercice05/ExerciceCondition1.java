88a708b91346d2be6fa3fdfd5b300e4a30d6ac1c
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-12 09:51:09 +0200

    exercice05 terminé

---
+++ exercices/exercice05/ExerciceCondition1.java
@@ -0,0 +1,15 @@
+package exercice05;
+
+public class ExerciceCondition1 {
+    public static void main(String[] args) {
+        int valeurATester = 5;
+
+        if (valeurATester > 0) {
+            System.out.println("la valeur est positif");
+            if (valeurATester < 0) {
+                System.out.println("la valeur est negatif");
+        }
+    }
+    
+}
+}
