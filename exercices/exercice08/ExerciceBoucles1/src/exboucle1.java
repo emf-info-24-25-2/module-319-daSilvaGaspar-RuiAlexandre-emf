033c7d0b7df744148cf8edab0df2de42aeef0d4d
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-09-19 11:34:27 +0200

    exercice08 términé

---
+++ exercices/exercice08/ExerciceBoucles1/src/exboucle1.java
@@ -0,0 +1,36 @@
+public class exboucle1 {
+    public static void main(String[] args) {
+        for (int i=1; i<6; i++){
+            System.out.println("Boucle for, itération " + i);
+            }
+            System.out.println(" ");
+            int l = 1;
+            while (l < 6) {
+                System.out.println("Boucle while, itération " + l);{
+                    l++;
+                }
+    }
+    System.out.println(" ");
+    int p = 1;
+    do {
+            System.out.println("Boucle do-while, itération " + p); p++;
+    
+    }while (p<6); 
+    System.out.println(" ");
+    for (int q=1; q<3; q++){
+        System.out.println("Boucle for, itération " + q);
+        }
+    
+
+        for (int w=4; w<6; w++){
+        System.out.println("Boucle for, itération " + w);
+}
+System.out.println(" ");
+int e = 1;
+while (e < 4) {
+    System.out.println("Boucle while, itération " + e);{
+        e++;
+    }
+}
+}
+}
