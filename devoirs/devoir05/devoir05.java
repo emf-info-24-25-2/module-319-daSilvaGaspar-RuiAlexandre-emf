7afb8a66a4649149695cfb9483b604075c6a8841
gaspar-alexandre-emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-10-07 19:00:23 +0200

    devoir05 terminé

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class devoir05 {
 /*tout dabord faudra crer un scanner pour scanner le nombre qu'on vas entrer et ensuite faudra aussi creer in chifre aleatoir qui est le chifre choisi par la machine
 et faudra faire une boucle while car tent que le chifre qu'on a entrer n'est pas egale au chifre choisie par la machine faudra continuer a jouer et quand on a trouver
-on sort de la boucle et on a gagner.*/
+on sort de la boucle et on a gagner .*/
     public static void main(String[] args) {
         int nbre = (int) (Math.random() * (100 - 1 + 1)) + 1;//ici on a crer un nombre aleatoire entre 1 et 100
         System.out.println("nous allons jouer a devine le nombre.");
