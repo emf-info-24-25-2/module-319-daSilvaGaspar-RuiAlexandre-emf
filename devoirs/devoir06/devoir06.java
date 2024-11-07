e25b812279fd29540f21ac20b3e9f46cddb11625
alexandre.gaspar.emf <RuiAlexandre.daSilvaGaspar@studentfr.ch>
2024-11-07 11:44:44 +0100

    devoir06 a terminé

---
+++ devoirs/devoir06/devoir06.java
@@ -0,0 +1,39 @@
+public class devoir06 {
+    public static final int TAILLE_TABLEAU = 20;
+    public static final int VALEUR_MIN = 0;
+    public static final int VALEUR_MAX = 50;
+    public static final int VALEUR_RECHERCHEE = 7;
+
+    public static int[] genereTableau(int max, int min, int taille) {
+        int[] tableau = new int[TAILLE_TABLEAU];
+
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = (int) (Math.random() * (VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
+        }
+        return tableau;
+    }
+
+    public static int rechercheMin(int[] tableau) {
+        int valeurMin = VALEUR_MAX + 1;
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] < valeurMin) {
+                valeurMin = tableau[i];
+            }
+        }
+        return valeurMin;
+    }
+    public static int rechercheMax(int[] tableau) {
+        int valeurMax = VALEUR_MIN - 1;
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i] > valeurMax) {
+                valeurMax = tableau[i];
+            }
+        }
+        return valeurMax;
+    
+    }
+    public static int rechercheValeur(int[] tableau, int valeur) {
+        
+            return valeur;
+    }
+}
